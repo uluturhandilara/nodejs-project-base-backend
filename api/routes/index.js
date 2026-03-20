699d737ef1bfeb45c565e203eafd83be55f010b3
uluturhandilara <dilarauluturhan@gmail.com>
2026-03-21 00:20:33 +0300

    feat:express-dynamic-routes

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,10 +1,14 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-const config = require('../config');
+const fs = require("fs");
 
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express', config });
-});
+let routes = fs.readdirSync(__dirname);
+
+for (let route of routes) {
+  if (route.includes(".js") && route != "index.js") {
+    router.use("/" + route.replace(".js", ""), require("./" + route));
+  }
+}
 
 module.exports = router;
