816a76f58c90083b1d12e95f4bcebb08be2ea981
uluturhandilara <dilarauluturhan@gmail.com>
2026-03-22 14:55:16 +0300

    feat:role-privileges-add-update-endpoint

--- api/routes/roles.js
+++ api/routes/roles.js
@@ -6,6 +6,7 @@ const RolePrivileges = require("../db/models/RolePrivileges");
 const Response = require("../lib/Response");
 const CustomError = require("../lib/Error");
 const Enum = require("../config/Enum");
+const role_privileges = require("../config/role_privileges");
 
 router.get("/", async (req, res) => {
   try {
@@ -27,6 +28,17 @@ router.post("/add", async (req, res) => {
         "role_name field must be filled",
       );
 
+    if (
+      !body.permissions ||
+      !Array.isArray(body.permissions) ||
+      body.permissions.length === 0
+    )
+      throw new CustomError(
+        Enum.HTTP_CODES.BAD_REQUEST,
+        "Validation error!",
+        "permissions field must be an array",
+      );
+
     let role = new Roles({
       role_name: body.role_name,
       is_active: true,
@@ -34,6 +46,16 @@ router.post("/add", async (req, res) => {
     });
 
     await role.save();
+
+    for (let i = 0; i < body.permissions.length; i++) {
+      let priv = new RolePrivileges({
+        role_id: role._id,
+        permission: body.permissions[i],
+        created_by: req.user?.id,
+      });
+
+      await priv.save();
+    }
 
     res.json(Response.successResponse({ success: true }));
   } catch (error) {
@@ -53,8 +75,50 @@ router.put("/update", async (req, res) => {
       );
 
     let updates = {};
+
     if (body.role_name) updates.role_name = body.role_name;
     if (typeof body.is_active === "boolean") updates.is_active = body.is_active;
+
+    if (
+      body.permissions &&
+      Array.isArray(body.permissions) &&
+      body.permissions.length > 0
+    ) {
+      // update sorgusu attğımızda yetkileri değiştirmek isteyebiliriz bu noktada bir kontrol daha koymamız gerekiyor
+      // bu kontrolde update etmeye çalıştığımız role ait olan yetkiler zaten db'de var
+      // bunların yeni gönderilen yetkilerle karşılaştırılması gerekiyor
+
+      // öncelikle update etmeye çalıştığımız role ait olan yetkileri buluyoruz
+      let permissions = await RolePrivileges.find({ role_id: body._id });
+      // yani db'de olan ama yeni gönderilen yetkilerde olmayan yetkileri buluyoruz
+      // body.permissions => ["category_view", "user_add"]
+      let removedPermissions = permissions.filter(
+        (x) => !body.permissions.includes(x.permission),
+      );
+      // body permissions'ta olan ama db'de olmayan yetkileri buluyoruz
+      // permissions => [{role_id: "abc", permission: "user_add", _id: "bcd"}];
+      let newPermissions = body.permissions.filter(
+        (x) => !permissions.map((p) => p.permission).includes(x),
+      );
+
+      if (removedPermissions.length > 0) {
+        await RolePrivileges.deleteMany({
+          _id: { $in: removedPermissions.map((x) => x._id) },
+        });
+      }
+
+      if (newPermissions.length > 0) {
+        for (let i = 0; i < newPermissions.length; i++) {
+          let priv = new RolePrivileges({
+            role_id: body._id,
+            permission: newPermissions[i],
+            created_by: req.user?.id,
+          });
+
+          await priv.save();
+        }
+      }
+    }
 
     await Roles.updateOne({ _id: body._id }, updates);
 
@@ -82,6 +146,10 @@ router.delete("/delete", async (req, res) => {
     let errorResponse = Response.errorResponse(error);
     res.status(errorResponse.code).json(errorResponse);
   }
+});
+
+router.get("/role_privileges", async (req, res) => {
+  res.json(role_privileges);
 });
 
 module.exports = router;
