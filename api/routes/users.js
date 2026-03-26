53e4000c5c94b06cd4c606d19bd7a0c4a3f55638
uluturhandilara <dilarauluturhan@gmail.com>
2026-03-26 23:39:12 +0300

    feat:user-register-endpoint

--- api/routes/users.js
+++ api/routes/users.js
@@ -126,4 +126,67 @@ router.delete("/delete", async (req, res) => {
   }
 });
 
+router.post("/register", async (req, res) => {
+  let body = req.body;
+  try {
+    let user = await Users.findOne({});
+
+    if (user)
+      throw new CustomError(
+        Enum.HTTP_CODES.CONFLICT,
+        "Validation error!",
+        "User already exists",
+      );
+
+    if (!body.email)
+      throw new CustomError(
+        Enum.HTTP_CODES.BAD_REQUEST,
+        "Validation error!",
+        "email field must be filled",
+      );
+
+    if (is.not.email(body.email))
+      throw new CustomError(
+        Enum.HTTP_CODES.BAD_REQUEST,
+        "Validation error!",
+        "email field must be a valid email address",
+      );
+
+    if (!body.password)
+      throw new CustomError(
+        Enum.HTTP_CODES.BAD_REQUEST,
+        "Validation error!",
+        "password field must be filled",
+      );
+
+    if (body.password.length < Enum.PASS_LENGTH)
+      throw new CustomError(
+        Enum.HTTP_CODES.BAD_REQUEST,
+        "Validation error!",
+        "password length must be greater than " +
+          Enum.PASS_LENGTH +
+          " characters",
+      );
+
+    let password = bcrypt.hashSync(body.password, bcrypt.genSaltSync(8), null);
+    await Users.create({
+      email: body.email,
+      password,
+      is_active: true,
+      first_name: body.first_name,
+      last_name: body.last_name,
+      phone_number: body.phone_number,
+    });
+
+    res
+      .status(Enum.HTTP_CODES.CREATED)
+      .json(
+        Response.successResponse({ success: true }, Enum.HTTP_CODES.CREATED),
+      );
+  } catch (error) {
+    let errorResponse = Response.errorResponse(error);
+    res.status(errorResponse.code).json(errorResponse);
+  }
+});
+
 module.exports = router;
