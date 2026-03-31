a6a5a8704b95de49ac7e0a36c9851d5c71085d77
uluturhandilara <dilarauluturhan@gmail.com>
2026-03-31 20:57:19 +0300

    feat:system-logs

--- api/routes/categories.js
+++ api/routes/categories.js
@@ -6,6 +6,7 @@ const Response = require("../lib/Response");
 const CustomError = require("../lib/Error");
 const Enum = require("../config/Enum");
 const AuditLogs = require("../lib/AuditLogs");
+const logger = require("../lib/logger/LoggerClass");
 
 router.get("/", async (req, res) => {
   try {
@@ -38,9 +39,11 @@ router.post("/add", async (req, res) => {
     await category.save();
 
     AuditLogs.info(req.user?.email, "Categories", "Add", category);
+    logger.info(req.user?.email, "Categories", "Add", category);
 
     res.json(Response.successResponse({ success: true }));
   } catch (error) {
+    logger.error(req.user?.email, "Categories", "Add", error);
     let errorResponse = Response.errorResponse(error);
     res.status(errorResponse.code).json(errorResponse);
   }
