32e63ddf358869e46015d5b28c39afea381c1c84
uluturhandilara <dilarauluturhan@gmail.com>
2026-04-03 15:56:38 +0300

    feat:auth-endpoint

--- api/db/models/Users.js
+++ api/db/models/Users.js
@@ -1,4 +1,8 @@
 const mongoose = require("mongoose");
+const { PASS_LENGTH, HTTP_CODES } = require("../../config/Enum");
+const bcrypt = require("bcrypt");
+const is = require("is_js");
+const CustomError = require("../../lib/Error");
 
 const schema = mongoose.Schema(
   {
@@ -18,7 +22,26 @@ const schema = mongoose.Schema(
   },
 );
 
-class Users extends mongoose.Model {}
+class Users extends mongoose.Model {
+  validPassword(password) {
+    return bcrypt.compareSync(password, this.password);
+  }
+
+  static validateFieldsBeforeAuth(email, password) {
+    if (
+      typeof password !== "string" ||
+      password.length < PASS_LENGTH ||
+      is.not.email(email)
+    )
+      throw new CustomError(
+        HTTP_CODES.UNAUTHORIZED,
+        "Validation Error",
+        "email or password wrong",
+      );
+
+    return null;
+  }
+}
 
 schema.loadClass(Users);
 module.exports = mongoose.model("users", schema);
