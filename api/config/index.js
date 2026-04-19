1f4e2d881777ccc73edd849dbf94bdc637bfadd3
uluturhandilara <dilarauluturhan@gmail.com>
2026-04-19 13:14:03 +0300

    refactor:jwt-security

--- api/config/index.js
+++ api/config/index.js
@@ -6,8 +6,7 @@ module.exports = {
     "mongodb://localhost:27017/project_base_backend",
   PORT: process.env.PORT || "3000",
   JWT: {
-    SECRET: "1234567890",
-    // "SECRET": process.env.JWT_SECRET || "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9eyJpZCI6IjY0NjY5OGUwZTUzYmI4MWVhYzlmYTc4NSIsImV4cCI6MTQ5NDg5NDk2NDMyMDAwfQv0SJjaBuR7SrLmvhVWxAiKgz4T4_rX55wHlD9CfBW10lmYTc4NSIsImV4cCI6MTQ5NDg5NDk2NDMyiLCJhbGciOiJI",
+    SECRET: process.env.JWT_SECRET || "",
     EXPIRE_TIME: !isNaN(parseInt(process.env.TOKEN_EXPIRE_TIME))
       ? parseInt(process.env.TOKEN_EXPIRE_TIME)
       : 24 * 60 * 60, // 86400
